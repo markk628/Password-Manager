2c23d083128d379d24a526f7574eee6a02483b98
Donghyun <markk628@gmail.com>
2020-01-07 15:49:41 -0800

    added base.html, navbar.html, requiremnet.txt using templates

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
+app = Flask(__name__)
+
+# home page
+@app.route('/')
+def pm_index():
+    return render_template('pm_index.html', passwords=passwords.find())
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
